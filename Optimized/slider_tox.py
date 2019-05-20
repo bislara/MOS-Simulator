d93960f4eb8ca2c650cc21ce8867901d9372aa69
bislara <biswajeetsahoo54@gmail.com>
2019-05-20 12:31:11 +0530

    Slider for Tox and NA

--- Optimized/slider_tox.py
+++ Optimized/slider_tox.py
@@ -6,13 +6,21 @@ from matplotlib.widgets import Slider  # import the Slider widget
 
 
 #variable declaration
-global Vgb_1,Vgb_2,Phi_g,tox,NA,ND,Condition,r
+global Vgb_1,Vgb_2,Phi_m,tox,NA,ND,Condition,r
 
 print("Welcome !!!")
 print("Enter all the values in the MKS system")
 
 fig, ax = plt.subplots()
-plt.subplots_adjust(left=0.1, bottom=0.15)
+plt.subplots_adjust(left=0.1, bottom=0.25)
+
+
+#user input
+Vgb_1=float(input("Enter the value of starting value of Vgb :\n"))
+Vgb_2=float(input("Enter the value of end value of Vgb :\n"))
+#NA=float(input("Enter the value of NA in per m^3 :"))
+#tox=float(input("Enter the value of oxide thickness in nm :"))
+#Phi_m=float(input("Enter the value of Phi_m :"))
 
 
 #constants initialize
@@ -33,24 +41,16 @@ colour_count=1
 colours={1:'b',2:'g',3:'r',4:'c',5:'m',6:'y',7:'k'}
 
 tox=2*10**(-9)
+#tox=tox*10**(-9)
 NA=5*10**23
-Phi_g=0.56
+Phi_m=0.56
 ND=0
 
 
 Es=ks*Eo		
-Eox=kox*Eo		#permittivity
-
-No=(Ni**2)/NA
-Po=NA
-Shi_F=Phi_t*log((NA)/(Ni)) 	
-n=2*Shi_F+Phi_t*6	
-Vfb=-0.56-Shi_F
+Eox=kox*Eo		
 
 
-#user input
-Vgb_1=float(input("Enter the value of starting value of Vgb :\n"))
-Vgb_2=float(input("Enter the value of end value of Vgb :\n"))
 
 
 #to take 0.1 steps in Vgb
@@ -59,6 +59,11 @@ for i in range(int(Vgb_1),int(Vgb_2*10)):
 
 
 for Vgb in r:
+	Po=NA
+	No=(Ni**2)/NA
+	Shi_F=Phi_t*log((NA)/(Ni)) 	
+	n=2*Shi_F+Phi_t*6	
+	Vfb=-Phi_m-Shi_F
 	Cox=Eox/tox
 	gm=(sqrt(2*q*Es*NA))/(Cox)
 	f=(-gm/2 + sqrt((gm)**2)/4 + Vgb - Vfb )**2  
@@ -73,31 +78,46 @@ plt.ylim(0,1.2)
 plt.xlim(0,1.5) 
 
 plt.title('Vgb VS SHI graph') 
+plt.legend()
 
 plt.xlabel('Vgb value') 
 plt.ylabel('SHI value')
 
 graph_plot,= plt.plot(V_list, Y_list,color ='r',label="tox=2nm")
 
-axSlider1= plt.axes([0.1,0.04,0.8,0.03])
+
+
+axSlider1= plt.axes([0.1,0.12,0.6,0.03])
+
 #slder1 = Slider(axSlider1, 'Slider 1', valmin=0, valmax=100)
-slder2 = Slider(			ax=axSlider1,
-					label='Slider',
+slider1 = Slider(			ax=axSlider1,
+					label='Tox',
 					valmin=1*10**(-9),
 					valmax=5*10**(-9),
-					valinit=2*10**(-9),
-					valfmt='%1.2f',
+					valinit=tox,
+					valfmt='tox is '+'%1.11f'+ ' in m',
 					color="green")
 
 
-def val_update(val):
+
+axSlider2= plt.axes([0.1,0.05,0.6,0.03])
+slider2 = Slider(axSlider2,'NA', valmin=1, valmax=20,valinit=NA/(10**23),valfmt='NA is '+'%1.2f'+ ' in 10**23')
+
+
+def val_update_tox(val):
+	global tox,NA
 	Y_list=[]
 	V_list=[]
-	tox=slder2.val
-	global Phi_t,q,Eo,ks,kox,Ni,NA,Phi_g,ND,Es,No,Po,Shi_f,n,Vfb,Vgb_1,Vgb_2,r
+	tox=slider1.val
+	print("NA is ",NA)
+	print("tox is ",tox)	
+	Po=NA
+	No=(Ni**2)/NA
+	Shi_F=Phi_t*log((NA)/(Ni)) 	
+	n=2*Shi_F+Phi_t*6	
+	Vfb=-Phi_m-Shi_F
 
-	Cox=Eox/tox
-	#print("Cox is ",Cox)		
+	Cox=Eox/tox		
 	gm=(sqrt(2*q*Es*NA))/(Cox)
 
    	for Vgb in r:
@@ -107,16 +127,74 @@ def val_update(val):
 	#	print("the val is : ",val)
 		Y_list.append(val)
 		V_list.append(Vgb)
+	#plt.plot(V_list, Y_list,color ='r',label="tox= "+tox)
 	
 	graph_plot.set_ydata(Y_list)
-	graph_plot.set_xdata(V_list)
+	#graph_plot.set_xdata(V_list)
 	plt.draw          # redraw the plot
 
 
-slder2.on_changed(val_update)
+def val_update_NA(val):
+	global tox,NA,Phi_m
+	Y_list=[]
+	V_list=[]
+	NA=(slider2.val)*10**23
+	print("NA is ",NA)
+	print("tox is ",tox)	
+	Po=NA
+	No=(Ni**2)/NA
+	Shi_F=Phi_t*log((NA)/(Ni)) 	
+	n=2*Shi_F+Phi_t*6	
+	Vfb=-Phi_m-Shi_F
+
+	Cox=Eox/tox
+	gm=(sqrt(2*q*Es*NA))/(Cox)
+
+   	for Vgb in r:
+		f=(-gm/2 + sqrt((gm)**2)/4 + Vgb - Vfb )**2  	
+		x0= min(f,n)
+		val=newtonRaphson(Vgb,x0,Vfb,NA,ND,Phi_t,q,Es,Cox,No,Po)  
+		Y_list.append(val)
+		V_list.append(Vgb)
+	
+	graph_plot.set_ydata(Y_list)
+	#graph_plot.set_xdata(V_list)
+	plt.draw          # redraw the plot
+
+
+def val_update_Phi(val):
+	global tox,NA,Phi_m
+	Y_list=[]
+	V_list=[]
+	NA=(slider3.val)*10**23
+	print("NA is ",NA)
+	print("tox is ",tox)	
+	Po=NA
+	No=(Ni**2)/NA
+	Shi_F=Phi_t*log((NA)/(Ni)) 	
+	n=2*Shi_F+Phi_t*6	
+	Vfb=-Phi_m-Shi_F
+
+	Cox=Eox/tox
+	gm=(sqrt(2*q*Es*NA))/(Cox)
+
+   	for Vgb in r:
+		f=(-gm/2 + sqrt((gm)**2)/4 + Vgb - Vfb )**2  	
+		x0= min(f,n)
+		val=newtonRaphson(Vgb,x0,Vfb,NA,ND,Phi_t,q,Es,Cox,No,Po)  
+		Y_list.append(val)
+		V_list.append(Vgb)
+	
+	graph_plot.set_ydata(Y_list)
+	#graph_plot.set_xdata(V_list)
+	plt.draw          # redraw the plot
+
+
+
+slider1.on_changed(val_update_tox)
+slider2.on_changed(val_update_NA)
+slider3.on_changed(val_update_Phi)
 
 plt.show()
-
-
 
 
